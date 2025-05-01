a3c057fe7008f8d5823419e807eeb65f6c8fab8f
henry <henry_tene@hotmail.com>
2025-05-01 15:56:51 -0500

    :sparkles: Se agrega formulario para registro

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,13 +26,14 @@
 
     </header>
     <main class = "container mx-auto mt-10">
-        <h2 class="text-3xl font-black text-center mb-10">
+        <h2 class="font-black text-center text-3xl mb-10">
             @yield('titulo')
         </h2>
         @yield('contenido')
 
     </main>
-    <footer class="text-center p-5 text-gray-500 font-bold uppercase mt-10">
+
+    <footer class="mt-10 text-center p-5 text-gray-500 font-bold uppercase">
         DevStagram - Todos los derechos reservados {{ now()->year }}
     </footer>
 </body>
