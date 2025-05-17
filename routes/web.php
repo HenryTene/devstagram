9c29801ca60908b465e2c3864d07f2c82a608aa9
Henry Tene <henry_tene@hotmail.com>
2025-05-17 15:53:50 -0500

    :sparkles: Se agrega el cierre de sesion al enlace

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,5 +16,6 @@ Route::post('/register', [RegisterController::class, 'store']);
 
 Route::get('/login', [LoginController::class, 'index'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
+Route::get('/logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/muro', [PostController::class, 'index'])->name('posts.index');
