4d9b21243c6551b34e9746e796ff01a8020f8986
Henry Tene <henry_tene@hotmail.com>
2025-05-23 13:40:18 -0500

    :sparkles: Se agrega configuracion adicional para trabajar con dropzone

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -8,7 +8,7 @@
 @section('contenido')
     <div class="md:flex md:items-center">
         <div class="md:w-1/2 px-10 ">
-            <form action="dropzone" class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center  items-center " id="dropzone">
+            <form action="/IMAGENES" id="dropzone" class="dropzone border-dashed border-2 w-full h-96 rounded flex flex-col justify-center  items-center " >
 
             </form>
         </div>
