7fdbe44ea0530856e9e47629cf52aa2b0847f7f1
Henry Tene <henry_tene@hotmail.com>
2025-05-22 19:45:54 -0500

    :sparkles: Se agrega configuracion para trabajar con dropzone

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-import './bootstrap';
+import Dropzone from 'dropzone';
+
+Dropzone.autoDiscover = false;
+
+const dropzone = new Dropzone('#my-dropzone', {
+    dictDefaultMessage: 'Sube aquí tu imagen',
+    acceptedFiles: '.png, .jpg, .jpeg, .gif',
+    addRemoveLinks: true,
+    dicRemoveFile: 'Eliminar archivo',
+    maxFiles: 2,
+    uploadMultiple: false,
+});
