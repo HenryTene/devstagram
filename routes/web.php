17246fadf0260b93f4820c34334dd91e5035f179
Henry Tene <henry_tene@hotmail.com>
2025-05-24 13:49:17 -0500

    :sparkles: Se agrega un controlador para subir imagenes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\ImagenController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -20,3 +21,5 @@ Route::post('/logout', [LogoutController::class, 'store'])->name('logout');
 
 Route::get('/{user:username}', [PostController::class, 'index'])->name('posts.index');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
+
+Route::post('/imagenes',[ImagenController::class, 'store'])->name('imagenes.store');
