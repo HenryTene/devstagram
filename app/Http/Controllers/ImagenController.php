3ba0f97d365ceadd37d73b9678d0470c8ff5ac6e
Henry Tene <henry_tene@hotmail.com>
2025-05-29 18:54:20 -0500

    :sparkles: Se agrega funcion para almacenar imagenes en servidor

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -3,14 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
+use Intervention\Image\ImageManager;
+use Intervention\Image\Drivers\Gd\Driver;
 
 class ImagenController extends Controller
 {
     //
     public function store(Request $request)
     {
+        $manager = new ImageManager(new Driver());
         //return "Imagen Controller";
         $imagen = $request->file('file');
-        return response()->json(['imagen'=>"Probando respuesta"]);
+        $nombreImagen = Str::uuid() . '.' . $imagen->extension();
+        $imagenServidor = $manager->read($imagen);
+        $imagenServidor->scale(1000, 1000);
+        $imagenPath = public_path('uploads') . '/' . $nombreImagen;
+        $imagenServidor->save($imagenPath);
+        return response()->json(['imagen'=>$nombreImagen]);
     }
 }
