d62a6c7d6022c831f905f2bb02db74dcda9c07d6
Henry Tene <henry_tene@hotmail.com>
2025-06-01 12:23:00 -0500

    :sparkles: Se agrega validaciones adicionales para subida de imagenes

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,6 +9,19 @@ const dropzone = new Dropzone("#dropzone", {
     dicRemoveFile: "Eliminar archivo",
     maxFiles: 1,
     uploadMultiple: false,
+
+    init: function () {
+        if (document.querySelector('[name="imagen"]').value.trim()) {
+            const imagenPublicada = {};
+            imagenPublicada.size = 1234;
+            imagenPublicada.name = document.querySelector('[name="imagen"]').value;
+
+            this.options.addedfile.call(this, imagenPublicada);
+            this.options.thumbnail.call(this, imagenPublicada, `/uploads/${imagenPublicada.name}`);
+            imagenPublicada.previewElement.classList.add("dz-success");
+            imagenPublicada.previewElement.classList.add("dz-complete");
+        }
+    }
 });
 
 
@@ -19,4 +32,6 @@ dropzone.on("success", function (file, response) {
 });
 
 
-dropzone.on("removedfile", function () {});
+dropzone.on("removedfile", function () {
+    document.querySelector('[name="imagen"]').value = "";
+});
