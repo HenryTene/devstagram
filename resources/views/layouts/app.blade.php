a7b4e445b794257127e123a0e1b994c528ba0312
henry <henry_tene@hotmail.com>
2025-06-04 17:03:51 -0500

    :hammer: Se actualiza codigo para los estilos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,8 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @stack('styles')
     <title>DevStagram - @yield('titulo')</title>
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
 </head>
 
 <body class="bg-gray-100">
