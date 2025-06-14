aedea32ec7cbd3c96641533664758e83dc748f92
HenryTene <henry_tene@hotmail.com>
2025-06-14 14:19:04 -0500

    :sparkles: se alamacenan los datos en la BD

--- app/Http/Controllers/ComentarioController.php
+++ app/Http/Controllers/ComentarioController.php
@@ -2,15 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Post;
+use App\Models\Comentario;
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Support\Facades\Auth;
 
 class ComentarioController extends Controller
 {
-    public function store(){
-       // Validar comentario
+    public function store(Request $request, User $user, Post $post)
+    {
+        // Validar comentario
+        $request->validate([
+            'comentario' => 'required|min:5|max:255',
+        ]);
 
        //Almacenar el resultado
+       Comentario::create([
+           'user_id' => Auth::user()->id,
+           'post_id' => $post->id,
+           'comentario' => $request->comentario,
+       ]);
 
-       //Imprimir un mensaje
+       //Imprimir un
+         return back()->with('mensaje', 'Comentario publicado correctamente');
     }
 }
--- app/Models/Comentario.php
+++ app/Models/Comentario.php
@@ -3,8 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Comentario extends Model
 {
     //
+    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'post_id',
+        'comentario',
+    ];
 }
