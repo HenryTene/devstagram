f7b8c4dac50fc137b157284f78fe37326f5e5d0b
HenryTene <henry_tene@hotmail.com>
2025-06-23 17:17:34 -0500

    :sparkles: implementar actualizacion de datos de perfil

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
 
 class PerfilController extends Controller
 {
@@ -19,13 +22,35 @@ class PerfilController extends Controller
         return view('perfil.index');
     }
 
-     public function store(Request $request){
+    public function store(Request $request)
+
+    {
 
         //Modificar el request
         $request->request->add(['username' => Str::slug($request->username)]);
-        $this->validate($request,[
-                    'username'=> ['required','unique:users,username,'.Auth::user()->id,'min:3','max:20','not_in:devstagram, twitter, facebook, instagram, youtube, linkedin,editar_perfil'],
-                ]);
+        $this->validate($request, [
+            'username' => ['required', 'unique:users,username,' . Auth::user()->id, 'min:3', 'max:20', 'not_in:devstagram, twitter, facebook, instagram, youtube, linkedin,editar_perfil'],
+        ]);
 
+        if ($request->imagen) {
+
+
+            $manager = new ImageManager(new Driver());
+            $imagen = $request->file('imagen');
+            $nombreImagen = Str::uuid() . '.' . $imagen->extension();
+            $imagenServidor = $manager->read($imagen);
+            $imagenServidor->scale(1000, 1000);
+            $imagenPath = public_path('perfiles') . '/' . $nombreImagen;
+            $imagenServidor->save($imagenPath);
+        }
+
+        //Guardar cambios
+        $usuario = User::find(Auth::user()->id);
+        $usuario->username = $request->username;
+        $usuario->imagen = $nombreImagen ?? $usuario->imagen; // Si no hay imagen, mantener la existente
+        $usuario->save();
+
+        //Redireccionar
+        return redirect()->route('posts.index', $usuario->username)->with('mensaje', 'Perfil actualizado correctamente');
     }
 }
