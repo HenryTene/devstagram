193e8de22c1f8c51ba71904563935acb74bac061
HenryTene <henry_tene@hotmail.com>
2025-07-02 11:27:01 -0500

    :sparkles: agregar manejo de eventos Livewire

--- app/Livewire/LikePost.php
+++ app/Livewire/LikePost.php
@@ -7,6 +7,11 @@ use Livewire\Component;
 class LikePost extends Component
 {
     public $post;
+
+    public function like()
+    {
+        return "Desde Like";
+    }
     public function render()
     {
         return view('livewire.like-post');
