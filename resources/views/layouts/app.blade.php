6b93345d6db41902013253ec5c867531c2382cd3
HenryTene <henry_tene@hotmail.com>
2025-07-06 00:47:36 -0500

    :sparkles: cambiar icono de agregar post

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,51 +17,46 @@
                 DevStagram
             </a>
             @auth()
-                <nav class="flex gap-2 items-center">
-                    <a
-                        class="flex items-center gap-2 bg-white border p-2 text-gray-600 rounded-lg text-sm uppercase font-bold cursor-pointer"
-                        href="{{ route('posts.create') }}"
-                    >
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6.827 6.175A2.31 2.31 0 0 1 5.186 7.23c-.38.054-.757.112-1.134.175C2.999 7.58 2.25 8.507 2.25 9.574V18a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9.574c0-1.067-.75-1.994-1.802-2.169a47.865 47.865 0 0 0-1.134-.175 2.31 2.31 0 0 1-1.64-1.055l-.822-1.316a2.192 2.192 0 0 0-1.736-1.039 48.774 48.774 0 0 0-5.232 0 2.192 2.192 0 0 0-1.736 1.039l-.821 1.316Z" />
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 12.75a4.5 4.5 0 1 1-9 0 4.5 4.5 0 0 1 9 0ZM18.75 10.5h.008v.008h-.008V10.5Z" />
+            <nav class="flex gap-2 items-center">
+                <a class="flex items-center gap-3 bg-blue-500 hover:bg-blue-600 border p-2 text-white rounded-lg text-sm uppercase font-bold cursor-pointer"
+                    href=" {{ route('posts.create') }}">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
+                        <path fill-rule="evenodd"
+                            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25ZM12.75 9a.75.75 0 0 0-1.5 0v2.25H9a.75.75 0 0 0 0 1.5h2.25V15a.75.75 0 0 0 1.5 0v-2.25H15a.75.75 0 0 0 0-1.5h-2.25V9Z"
+                            clip-rule="evenodd" />
                     </svg>
+                    Crear
+                </a>
+                <a class="font-bold text-gray-600" href="{{ route('posts.index', auth()->user()->username) }}">
+                    Hola:
+                    <span class="font-normal">
+                        {{ auth()->user()->username }}
+                    </span>
+                </a>
 
-                        Crear
-                    </a>
-                    <a
-                        class="font-bold text-gray-600"
-                        href="{{ route('posts.index', auth()->user()->username) }}"
-                    >
-                        Hola:
-                        <span class="font-normal">
-                            {{ auth()->user()->username }}
-                        </span>
-                    </a>
-
-                    <form method="POST" action="{{ route('logout') }}" >
-                        @csrf
-                        <button  type="submit" class="font-bold uppercase text-gray-600" >
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="font-bold uppercase text-gray-600">
                         Cerrar Sesión
-                        </button>
-                    </form>
+                    </button>
+                </form>
 
-                </nav>
+            </nav>
             @endauth
 
             @guest()
-                <nav class="flex gap-2 items-center">
-                    <a class="font-bold uppercase text-gray-600" href="{{route('login')}}" >
-                        Login
-                    </a>
-                    <a href="{{route('register')}}" class="font-bold uppercase text-gray-600" >
-                        Crear cuenta
-                    </a>
-                </nav>
+            <nav class="flex gap-2 items-center">
+                <a class="font-bold uppercase text-gray-600" href="{{route('login')}}">
+                    Login
+                </a>
+                <a href="{{route('register')}}" class="font-bold uppercase text-gray-600">
+                    Crear cuenta
+                </a>
+            </nav>
             @endguest
         </div>
     </header>
-    <main class = "container mx-auto mt-10">
+    <main class="container mx-auto mt-10">
         <h2 class="font-black text-center text-3xl mb-10">
             @yield('titulo')
         </h2>
